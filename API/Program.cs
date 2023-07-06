789231073f6f64955968606262b34feed8995618
Dan Serbu <danandreis@yahoo.com>
2023-07-06 20:49:35 +0300

    firsta commit

--- API/Program.cs
+++ API/Program.cs
@@ -13,9 +13,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
 
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
